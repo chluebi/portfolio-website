f2dca1aac23bc0239a8570a79d40f6ca71382422
chluebi <42419603+chluebi@users.noreply.github.com>
2024-10-28 22:06:11 +0100

    parsing metadata

--- backend/src/parse.rs
+++ backend/src/parse.rs
@@ -1,8 +1,27 @@
 use std::fs;
 use std::io::{self, Read};
 use std::path::Path;
+use regex::Regex;
+use serde::Deserialize;
 
-use serde_json::{Error, Result};
+#[derive(Debug, Deserialize)]
+struct Metadata {
+    url: String,
+    year: u32,
+    languages: Vec<String>,
+    keywords: Vec<String>,
+}
+
+
+fn extract_metadata(content: &str) -> Option<&str> {
+    let re = Regex::new(r"(?s)^---\n(.*?)\n---").unwrap();
+    re.captures(content).and_then(|caps| caps.get(1).map(|m| m.as_str()))
+}
+
+fn extract_content(content: &str) -> Option<&str> {
+    let re = Regex::new(r"(?s)^---\n(.*?)\n---\n(.*)").unwrap();
+    re.captures(content).and_then(|caps| caps.get(2).map(|m| m.as_str()))
+}
 
 
 pub fn read() {
@@ -32,12 +51,28 @@ pub fn read() {
                 let mut contents = String::new();
                 if let Err(error) = file.read_to_string(&mut contents) {
                     eprintln!("Error reading file: {}", error);
-                    continue;  
-
-
+                    continue;
                 }
 
-                println!("File contents:\n{}", contents);
+                let metadata_str = match extract_metadata(&contents) {
+                    Some(m) => m,
+                    None => {
+                        eprintln!("No metadata in: {}", path.display());
+                        continue;
+                    }
+                };
+                let metadata: Metadata = serde_yaml::from_str(&metadata_str).unwrap();
+                let content = match extract_content(&contents) {
+                    Some(c) => c,
+                    None => {
+                        eprintln!("No content in: {}", path.display());
+                        continue;
+                    }
+                };
+                
+                println!("File metadata: \n{}", metadata_str);
+                println!("File content:\n{}", content);
+                println!("File url: {}", metadata.url)
             }
         }
         Err(e) => {
