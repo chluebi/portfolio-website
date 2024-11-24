510ed951df9d0e5a01262dd7cd2af4055ffeb197
chluebi <42419603+chluebi@users.noreply.github.com>
2024-11-24 13:20:09 +0100

    remove old search bar suggestion rotation

--- frontend/src/searchbar.ts
+++ frontend/src/searchbar.ts
@@ -18,84 +18,8 @@ export function setupSearch(searchBox: HTMLInputElement, searchCallback: SearchC
 
   const completionDiv = document.querySelector<HTMLDivElement>("#search-completion");
 
-  const suggestions = ['Python', 'Rust', 'Java', 'C++'];
-
-  enum Mode {
-    Start,
-    Suggestions,
-    Focused,
-    Unfocused,
-    Typing
-  }
-
-  let mode: Mode = Mode.Start;
-
-  let suggestionIndex = 0;
-  let typingTimer: number;
-  let inactivityTimer: number;
-  const inactivityDelay = 3000;
-
-  function typeSuggestion(suggestion: string) {
-    searchBox.value = '';
-    searchBox.classList.add('typing-animation');
-  
-    let i = 0;
-    typingTimer = window.setInterval(() => {
-      if (mode != Mode.Suggestions) {
-        clearInterval(typingTimer);
-        searchBox.classList.remove('typing-animation');
-        searchBox.value = '';
-        return;
-      }
-
-      if (i < suggestion.length) {
-        searchBox.value += suggestion.charAt(i);
-        i++;
-      } else {
-        clearInterval(typingTimer);
-        searchBox.classList.remove('typing-animation');
-      }
-    }, 80);
-  }
-
-  function cycleSuggestion() {
-    searchBox.value = "";
-    searchBox.classList.remove('strong-text');
-    const suggestion = suggestions[suggestionIndex] + " projects...";
-    typeSuggestion(suggestion);
-  
-    suggestionIndex = (suggestionIndex + 1) % suggestions.length;
-  }
-
-  function resetInactivityTimer() {
-    clearTimeout(inactivityTimer);
-    inactivityTimer = window.setTimeout(() => {
-      if (mode == Mode.Start || mode == Mode.Focused || mode == Mode.Suggestions) {
-        mode = Mode.Suggestions;
-        cycleSuggestion();
-        resetInactivityTimer();
-        return;
-      }
-      resetInactivityTimer();
-    }, inactivityDelay);
-  }
-
-  searchBox.addEventListener('focus', () => {
-    if (mode == Mode.Focused || mode == Mode.Typing || mode == Mode.Unfocused) {
-      return;
-    }
-    mode = Mode.Focused;
-    searchBox.value = "";
-    searchBox.classList.add('strong-text');
-  });
-
-  searchBox.addEventListener('blur', () => {
-    mode = Mode.Unfocused;
-    resetInactivityTimer();
-  });
-
   searchBox.addEventListener('keydown', (event) => {
-    if (event.key == "Tab" && mode == Mode.Typing) {
+    if (event.key == "Tab") {
       event.preventDefault();
       if (completionDiv) {
         searchBox.value = completionDiv.innerHTML;
@@ -105,10 +29,6 @@ export function setupSearch(searchBox: HTMLInputElement, searchCallback: SearchC
   })
 
   searchBox.addEventListener('keyup', (event) => {
-    if (mode == Mode.Suggestions) {
-      searchBox.value = "";
-    }
-    mode = Mode.Typing;
     searchBox.classList.add('strong-text');
 
     if (completionDiv) {
@@ -132,14 +52,10 @@ export function setupSearch(searchBox: HTMLInputElement, searchCallback: SearchC
 
   function hardSetSearch(s: String) {
     searchBox.focus();
-    mode = Mode.Typing;
     searchBox.classList.add('strong-text');
 
     searchBox.value = s.toString();
   }
-
-
-  resetInactivityTimer();
 
   return hardSetSearch;
 }
